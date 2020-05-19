302cad8893d7f505d341c2cf8a0738829050c328
warrickct <warrickct@gmail.com>
2020-05-20 10:15:32 +1200

    listing project ids

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,18 +2,38 @@
 import React from 'react';
 import './Project.css';
 
-// class Project extends React.Component {
-//     render() {
-//         return (
-//             <div>this is the project page.{projectId}</div>
-//         )
-//     }
-// }
+const projectList = (experiments) => {
+    experiments.forEach(experiment => {
+        return (
+            <div>experiment.name</div>
+        )
+    });
+}
 
+const experimentList = (projectId) => {
+    let experiments = []
+    for (let i = 0; i < 20; i++) {
+        experiments.push({
+            name: 'project__' + projectId + "_experiment__" + i,
+            description: "This is the description for experiment #" + i,
+        });
+    }
+    return experiments;
+}
 
 const Project = ({ projectId }) => {
+
     return (
-        <div>project id for: {projectId}</div>
+        <div>
+            <div>project id for: {projectId}</div>
+            <ul>
+                {
+                    experimentList(projectId).map(item => {
+                    return <li>{item.name}</li>;
+                    })
+                }
+            </ul>
+        </div>
     )
 }
 
